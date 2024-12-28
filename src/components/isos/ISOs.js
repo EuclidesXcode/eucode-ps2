459edff6396dc0f7a78af4827cd49d70636b616c
Euclides Silva <euclidessilva@Euclidess-MacBook-Air.local>
2024-12-28 16:43:07 -0300

    add games component with images and descriptions; update package.json for @mui/styles

--- src/components/isos/ISOs.js
+++ src/components/isos/ISOs.js
@@ -1,29 +1,26 @@
 import React, { useState } from 'react';
-import { Card, CardContent, CardMedia, Typography, Grid, Box } from '@material-ui/core';
-import { Button } from '@mui/material';
-import { makeStyles } from '@material-ui/core/styles';
-import godOfWar from '../../assets/god-of-war.jpeg';
-import needForSpeed from '../../assets/need-under-2.jpeg';
-import shadowOfTheColossus from '../../assets/shadow-of-the-colossus.jpeg';
-import redDeadRevolver from '../../assets/red-dead-revolver.jpg';
-import spiderMen2 from '../../assets/spider-man-2.jpg';
-import sniperElite from '../../assets/sniper-elite.webp';
+import { Grid, Card, CardMedia, CardContent, Typography, Button, TextField } from '@mui/material';
+import { makeStyles } from '@mui/styles';
+import games from './games';
 
 const useStyles = makeStyles((theme) => ({
     card: {
-        transition: 'transform 0.3s, box-shadow 0.3s',
-        '&:hover': {
-            transform: 'scale(1.05)',
-            boxShadow: theme.shadows[6],
-            zIndex: 1,
-        },
         display: 'flex',
         flexDirection: 'column',
-        height: '100%',
+        justifyContent: 'space-between',
+        height: 420,
+        width: '100%',
+        transition: 'transform 0.3s ease-in-out',
+        zIndex: 1,
+    },
+    cardHovered: {
+        transform: 'scale(1.05)',
+        zIndex: 2,
     },
     cardMedia: {
-        height: 400,
-        objectFit: 'cover',
+        height: 0,
+        paddingTop: '56.25%', // 16:9
+        backgroundSize: 'contain',
     },
     cardContent: {
         flexGrow: 1,
@@ -37,48 +34,61 @@ const useStyles = makeStyles((theme) => ({
         left: 0,
         width: '100%',
         height: '100%',
-        backgroundColor: 'rgba(0, 0, 0, 0.2)',
-        zIndex: 0,
+        backgroundColor: 'rgba(0, 0, 0, 0.5)',
+        zIndex: 1,
     },
     button: {
         alignSelf: 'center',
         marginTop: 'auto',
     },
+    searchField: {
+        marginBottom: '20px',
+    },
 }));
 
 const ISOs = () => {
     const classes = useStyles();
-    const [hovered, setHovered] = useState(false);
+    const [hovered, setHovered] = useState(null);
+    const [searchTerm, setSearchTerm] = useState('');
 
-    const games = [
-        { title: 'God of War', description: 'Embarque em uma jornada épica com Kratos nesta aventura cheia de ação ambientada na Grécia antiga.', image: godOfWar, link: 'https://drive.google.com/file/d/1mKWcU-m4CXj37QPlUyDxY4O7vkURNQNN/view?usp=share_link' },
-        { title: 'Need for Speed Underground 2', description: 'Corra pelas ruas nesta emocionante sequência, com carros personalizáveis e corridas de rua intensas.', image: needForSpeed, link: 'https://drive.google.com/file/d/11mW64yMp8M8CjvXu6QMo1FLh6V4aL5P9/view?usp=share_link' },
-        { title: 'Shadow Of The Colossus', description: 'Explore um vasto mundo misterioso e derrote colossos gigantes nesta aclamada aventura de ação.', image: shadowOfTheColossus, link: 'https://drive.google.com/file/d/1xs2QiA8nEf4rHxK71-sWuxhu7fOZAJAv/view?usp=share_link' },
-        { title: 'Red Dead Revolver', description: 'Viva o Velho Oeste em uma aventura cheia de ação e tiroteios intensos.', image: redDeadRevolver, link: 'https://drive.google.com/file/d/1dNw2aaGVHqPMp-tgm2Tr0UOI_M8eJGEW/view?usp=share_link' },
-        { title: 'Spider-Man 2', description: 'Balance pelas ruas de Nova York como o Homem-Aranha em uma emocionante aventura.', image: spiderMen2, link: 'https://drive.google.com/file/d/19aGNJLZciPXYrvfz4C0iZkj7ScA6RFl5/view?usp=share_link' },
-        { title: 'Sniper Elite', description: 'Assuma o papel de um atirador de elite na Segunda Guerra Mundial e complete missões furtivas.', image: sniperElite, link: 'https://drive.google.com/file/d/17JzEBMKyUjMCBH31EKgo_pvhfqFzkIoL/view?usp=share_link' },
-    ];
+    // Ordenar jogos em ordem alfabética
+    const sortedGames = games.sort((a, b) => a.title.localeCompare(b.title));
+
+    // Filtrar jogos com base no termo de pesquisa
+    const filteredGames = sortedGames.filter(game =>
+        game.title.toLowerCase().includes(searchTerm.toLowerCase())
+    );
 
     return (
-        <div style={{ padding: 20 }}>
+        <div style={{ paddingTop: 20, paddingLeft: 200, paddingRight: 200 }}>
             <h1>Baixe gratuitamente todas as ISO`s da nossa comunidade!</h1>
             <h2>E o melhor de tudo, sem anúncios, sem enganação</h2>
             <p>Todas nossas ISOs estão no Goolge Drive, então voce será redirecionado ao Google Drive quando clicar em download! Não se preocupe, todas nossas ISOs são testadas antes de vir pra cá!</p>
-            {hovered && <div className={classes.overlay} />}
-            <Grid container spacing={10} style={{ padding: 50 }}>
-                {games.map((game, index) => (
-                    <Grid item key={index} xs={12} sm={4} md={3}>
+            <TextField
+                label="Pesquisar ISOs"
+                variant="outlined"
+                fullWidth
+                className={classes.searchField}
+                value={searchTerm}
+                onChange={(e) => setSearchTerm(e.target.value)}
+            />
+            {hovered !== null && <div className={classes.overlay} />}
+            <Grid container spacing={3} style={{ padding: 50 }}>
+                {filteredGames.map((game, index) => (
+                    <Grid item key={index} xs={12} sm={6} md={4} lg={3}>
                         <Card
-                            className={classes.card}
-                            onMouseEnter={() => setHovered(true)}
-                            onMouseLeave={() => setHovered(false)}
+                            className={`${classes.card} ${hovered === index ? classes.cardHovered : ''}`}
+                            onMouseEnter={() => setHovered(index)}
+                            onMouseLeave={() => setHovered(null)}
+                            sx={{ borderRadius: 5 }}
                         >
                             <CardMedia
                                 className={classes.cardMedia}
-                                component="img"
+                                component="div"
                                 alt={game.title}
                                 image={game.image}
                                 title={game.title}
+                                sx={{ marginTop: 2 }}
                             />
                             <CardContent className={classes.cardContent}>
                                 <div>
@@ -92,12 +102,12 @@ const ISOs = () => {
                                 <Button
                                     variant="contained"
                                     color="primary"
-                                    className={classes.button}
                                     href={game.link}
                                     target="_blank"
                                     rel="noopener noreferrer"
+                                    className={classes.button}
                                 >
-                                    Download Grátis
+                                    Download
                                 </Button>
                             </CardContent>
                         </Card>
---
+++ src/components/isos/games.js
@@ -0,0 +1,17 @@
+import godOfWar from '../../assets/god-of-war.jpeg';
+import needForSpeed from '../../assets/need-under-2.jpeg';
+import shadowOfTheColossus from '../../assets/shadow-of-the-colossus.jpeg';
+import redDeadRevolver from '../../assets/red-dead-revolver.jpg';
+import spiderMen2 from '../../assets/spider-man-2.jpg';
+import sniperElite from '../../assets/sniper-elite.jpg';
+
+const games = [
+    { title: 'God of War', description: 'Embarque em uma jornada épica com Kratos nesta aventura cheia de ação ambientada na Grécia antiga.', image: godOfWar, link: 'https://drive.google.com/file/d/1mKWcU-m4CXj37QPlUyDxY4O7vkURNQNN/view?usp=share_link' },
+    { title: 'Need for Speed Underground 2', description: 'Corra pelas ruas nesta emocionante sequência, com carros personalizáveis e corridas de rua intensas.', image: needForSpeed, link: 'https://drive.google.com/file/d/11mW64yMp8M8CjvXu6QMo1FLh6V4aL5P9/view?usp=share_link' },
+    { title: 'Shadow Of The Colossus', description: 'Explore um vasto mundo misterioso e derrote colossos gigantes nesta aclamada aventura de ação.', image: shadowOfTheColossus, link: 'https://drive.google.com/file/d/1xs2QiA8nEf4rHxK71-sWuxhu7fOZAJAv/view?usp=share_link' },
+    { title: 'Red Dead Revolver', description: 'Viva o Velho Oeste em uma aventura cheia de ação e tiroteios intensos.', image: redDeadRevolver, link: 'https://drive.google.com/file/d/1dNw2aaGVHqPMp-tgm2Tr0UOI_M8eJGEW/view?usp=share_link' },
+    { title: 'Spider-Man 2', description: 'Balance pelas ruas de Nova York como o Homem-Aranha em uma emocionante aventura.', image: spiderMen2, link: 'https://drive.google.com/file/d/19aGNJLZciPXYrvfz4C0iZkj7ScA6RFl5/view?usp=share_link' },
+    { title: 'Sniper Elite', description: 'Assuma o papel de um atirador de elite na Segunda Guerra Mundial e complete missões furtivas.', image: sniperElite, link: 'https://drive.google.com/file/d/17JzEBMKyUjMCBH31EKgo_pvhfqFzkIoL/view?usp=share_link' },
+];
+
+export default games;
