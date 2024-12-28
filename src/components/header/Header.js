0d451d6bc398c9c15aca9a390fd0f71884e1689a
Euclides Silva <euclidessilva@Euclidess-MacBook-Air.local>
2024-12-28 15:25:43 -0300

    visitors

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,16 +1,37 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import { Link } from 'react-router-dom';
+import { database } from '../../firebase';
+import { ref, get, increment, update } from 'firebase/database';
 
 const Header = () => {
+    const [visitors, setVisitors] = useState(0);
+
+    useEffect(() => {
+        const visitorsRef = ref(database, 'visitors');
+        get(visitorsRef).then((snapshot) => {
+            if (snapshot.exists()) {
+                const currentVisitors = snapshot.val();
+                setVisitors(currentVisitors);
+                update(visitorsRef, { count: increment(1) });
+            } else {
+                setVisitors(1);
+                update(visitorsRef, { count: 1 });
+            }
+        });
+    }, []);
+
     return (
         <AppBar position="static">
             <Toolbar>
                 <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
                     Eucode PS2 ISOs
+                </Typography>
+                <Typography variant="body1" component="div" sx={{ flexGrow: 1 }}>
+                    Visitantes: {visitors}
                 </Typography>
                 <Button color="inherit" component={Link} to="/isos">ISOs</Button>
                 <Button color="inherit" component={Link} to="/emulator">Emulador</Button>
