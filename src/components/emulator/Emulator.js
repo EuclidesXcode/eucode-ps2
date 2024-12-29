9b424f18ec7bbc45f108b5b02d1a791cf4e88edd
Euclides Silva <euclidessilva@Euclidess-MacBook-Air.local>
2024-12-29 11:44:58 -0300

    refactor emulator links for improved readability

--- src/components/emulator/Emulator.js
+++ src/components/emulator/Emulator.js
@@ -7,22 +7,22 @@ const Emulator = () => {
             <h1>Emulador</h1>
             <p>Ainda não tem o emulador do PS2?</p>
             <Typography variant="body1">
-                <h5>Windows</h5>
-                <Link href="https://drive.google.com/file/d/1dFcvk5Fgjd9eZ8ARem-P81-ytsbmn8-J/view?usp=share_link" target="_blank" rel="noopener noreferrer">
+                <h5>Windows -{'>'}<Link href="https://drive.google.com/file/d/1dFcvk5Fgjd9eZ8ARem-P81-ytsbmn8-J/view?usp=share_link" target="_blank" rel="noopener noreferrer">
                     Baixe aqui de graça
-                </Link>
+                </Link></h5>
+                
             </Typography>
             <Typography variant="body1">
-                <h5>Mac</h5>
-                <Link href="https://drive.google.com/file/d/1NB_ka_spyRtrNvRZyTA8ZCKNpzbgiGiz/view?usp=sharing" target="_blank" rel="noopener noreferrer">
+                <h5>Mac -{'>'} <Link href="https://drive.google.com/file/d/1NB_ka_spyRtrNvRZyTA8ZCKNpzbgiGiz/view?usp=sharing" target="_blank" rel="noopener noreferrer">
                     Baixe aqui de graça
-                </Link>
+                </Link></h5>
+                
             </Typography>
             <Typography variant="body1">
-                <h5>Linux</h5>
-                <Link href="https://drive.google.com/file/d/1keDU0w68SfrhyE0gAkdAA9Qf-Rzzdrex/view?usp=sharing" target="_blank" rel="noopener noreferrer">
+                <h5>Linux -{'>'} <Link href="https://drive.google.com/file/d/1keDU0w68SfrhyE0gAkdAA9Qf-Rzzdrex/view?usp=sharing" target="_blank" rel="noopener noreferrer">
                     Baixe aqui de graça
-                </Link>
+                </Link></h5>
+                
             </Typography>
             <p>Baixe aqui também a BIOS do emulador:</p>
             <Typography variant="body1">
